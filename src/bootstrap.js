52c608a4ea5c13fdbf94495338c1afeb16c279a7
Dave Cooper <dave.cooper@elephant.healthcare>
2021-09-24 14:35:37 +0100

    Fix eager consumption

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,1 +1,5 @@
-import("./bootstrap");
+import React from "react";
+import ReactDOM from "react-dom";
+import { Account } from "./Account";
+
+ReactDOM.render(<Account />, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Account } from "./Account";
-
-ReactDOM.render(<Account />, document.getElementById("root"));
+import("./bootstrap");
